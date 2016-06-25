87e3befb50031faa4130c18f12b9c9c319b109a8
gapoorva <gapoorva@umich.edu>
2016-06-25 12:14:55 -0600

    Home page visual updates

--- index.php
+++ index.php
@@ -43,27 +43,9 @@
       <div class="row masthead">
         <div class="row title-banner">
           <div col-xs-12>
-           <h1 class="text-center">Theta Gamma Chapter</h1>
-           <h2 class="text-center">University of Michigan</h2>
+           <img class="img-responsive" src="images/banner-title.png">
           </div>
         </div>
-        <!-- quote -->
-      <?php 
-        foreach($mastcontent as $i => $slide) {
-          echo "<div class='quote-block row ".$indexConfig['MastSlideShow']['QuoteClass'];
-          if ($i == 0)
-            echo ' '.$indexConfig['MastSlideShow']['QuoteToggleClass'];
-          echo "'>";
-      ?>
-          <div class="col-xs-12">
-            <h5 class="text-center lead opensans">"<?php echo $slide['quote'];?>"</h5>
-            <h4 class="text-center opensans"><?php echo $slide['credit'];?>, Member</h4>
-          </div>
-        </div>
-      <?php
-        }
-      ?>
-        <!-- End quote -->
 
         <!-- Masthead Images -->
       <?php
@@ -81,7 +63,7 @@
           <h3 class="text-center">About</h3>
           <p>Theta Tau is Professional Engineering Fraternity. As a group, we are dedicated to the professional and social development of our members into professionals that will enter the industry as strong, contributing members. Our chapter is known as Theta Gamma Chapter and is one of the largest chapters in the Country.</p>
           <p>Theta Tau is made up of smart, driven engineers who come from a diverse range of backgrounds and majors. In fact, this is one of our greatest strengths, since students at the University of Michigan pride themselves in the diversity of their school. Our chapter holds events weekly designed to instill a brotherhood among our members and develop ourselves as well as our college and University.</p>
-          <p> Are you interested in joining our chapter? Find more information on our <a href="rush.html">Rush Page</a>.</p>
+          <p class="text-center join-button"><b> <a class="btn btn-tht" href="rush.php">JOIN THETA TAU </a></b></p>
         </div>
         <div class="col-sm-6">
           <h3 class="text-center">History</h3>
